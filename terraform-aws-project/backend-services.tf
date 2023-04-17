83cf3542903a05fbc4ee801be0b2c48d4dd17457
sujanmj <Sujan.mj03@ymail.com>
2023-04-17 17:20:21 +0530

    rds

--- terraform-aws-project/backend-services.tf
+++ terraform-aws-project/backend-services.tf
@@ -1,4 +1,4 @@
-resource "aws_db_subnet_group" "vprofile-rds-subgrp" {
+resource "aws_db_subnet_group" "mjfile-rds-subgrp" {
   name       = "main"
   subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
   tags = {
@@ -6,13 +6,13 @@ resource "aws_db_subnet_group" "vprofile-rds-subgrp" {
   }
 }
 
-resource "aws_elasticache_subnet_group" "vprofile-ecache-subgrp" {
-  name       = "vprofile-ecache-subgrp"
+resource "aws_elasticache_subnet_group" "mjfile-ecache-subgrp" {
+  name       = "mjfile-ecache-subgrp"
   subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
 
 }
 
-resource "aws_db_instance" "vprofile-rds" {
+resource "aws_db_instance" "mjfile-rds" {
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "mysql"
@@ -25,27 +25,27 @@ resource "aws_db_instance" "vprofile-rds" {
   multi_az               = "false"
   publicly_accessible    = "false"
   skip_final_snapshot    = true
-  db_subnet_group_name   = aws_db_subnet_group.vprofile-rds-subgrp.name
-  vpc_security_group_ids = [aws_security_group.vprofile-backend-sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.mjfile-rds-subgrp.name
+  vpc_security_group_ids = [aws_security_group.mjfile-backend-sg.id]
 }
 
-resource "aws_elasticache_cluster" "vprofile-cache" {
-  cluster_id           = "vprofile-cache"
+resource "aws_elasticache_cluster" "mjfile-cache" {
+  cluster_id           = "mjfile-cache"
   engine               = "memcached"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.memcached1.5"
   port                 = 11211
-  security_group_ids   = [aws_security_group.vprofile-backend-sg.id]
-  subnet_group_name    = aws_elasticache_subnet_group.vprofile-ecache-subgrp.name
+  security_group_ids   = [aws_security_group.mjfile-backend-sg.id]
+  subnet_group_name    = aws_elasticache_subnet_group.mjfile-ecache-subgrp.name
 }
 
-resource "aws_mq_broker" "vprofile-rmq" {
-  broker_name        = "vprofile-rmq"
+resource "aws_mq_broker" "mjfile-rmq" {
+  broker_name        = "mjfile-rmq"
   engine_type        = "ActiveMQ"
   engine_version     = "5.15.0"
   host_instance_type = "mq.t2.micro"
-  security_groups    = [aws_security_group.vprofile-backend-sg.id]
+  security_groups    = [aws_security_group.mjfile-backend-sg.id]
   subnet_ids         = [module.vpc.private_subnets[0]]
 
   user {
