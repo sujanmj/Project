14e5972fc93ee292d0aee8ba7aa980b74c629051
sujanmj <Sujan.mj03@ymail.com>
2023-04-17 15:50:23 +0530

    s3

--- terraform-aws-vprofile-project/backend-s3.tf
+++ terraform-aws-vprofile-project/backend-s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terra-vprofile-state11"
+    bucket = "mj-terra-bucket"
     key    = "terraform/backend"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
